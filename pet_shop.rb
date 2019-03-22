ed5a8936f5d2cb17b274ac680dc03a7743efc91e
davemc85 <davemcallister@hotmail.co.uk>
2019-03-22 15:26:20 +0000

    11 tests complete

--- pet_shop.rb
+++ pet_shop.rb
@@ -14,3 +14,37 @@ end
 def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold]
 end
+
+def increase_pets_sold(pet_shop, number_sold)
+  pet_shop[:admin][:pets_sold] += number_sold
+  return pet_shop[:admin][:pets_sold]
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].length
+end
+
+def pets_by_breed(pet_shop, breed)
+  pets = []
+  for pet_new in pet_shop[:pets]
+    if pet_new[:breed] == breed
+      pets.push(pet_shop)
+    end
+  end
+  return pets
+end
+
+
+def find_pet_by_name(shop, pet_name)
+  for pet_new in shop[:pets]
+    if pet_new[:name] == pet_name
+      return pet_new
+    end
+  end
+  return nil
+end
+
+
+def remove_pet_by_name
+
+end
